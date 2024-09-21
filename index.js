9fffae76224282a353854aa89e919e226d2b18c3
Luis Miguel Trinidad Salvador <lmtrinidadsalvador@gmail.com>
2024-09-21 15:48:40 +0200

    uibuild

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 const app = express();
 app.use(cors())
 app.use(express.json());
+app.use(express.static('dist'))
 
 const morganFormatter = (tokens, req, res) => {
   const log = [
